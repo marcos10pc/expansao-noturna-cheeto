41dc0203f7cf6b635f3e94728d8a8f2dc6d0cfbb
marcos10pc <30839781+marcos10pc@users.noreply.github.com>
2025-03-02 15:09:57 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Expansão Noturna Cheeto
+// @namespace https://github.com/marcos10pc/expansao-noturna-cheeto
+// @supportURL      https://github.com/marcos10pc/expansao-noturna-cheeto
+// @version      2025-03-02
+// @description  busque comer cimento
+// @author       marcos10pc
+// @match        https://expansao.educacao.sp.gov.br/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=gov.br
+// @grant        none
+// @require https://raw.githubusercontent.com/marcos10pc/expansao-noturna-cheeto/refs/heads/main/imisstheoldkanye.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log("https://pixgg.com/marcos10pc"); 
+})();
