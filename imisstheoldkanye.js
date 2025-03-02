a83829365fc7a3c7ab69c96dcfd5566720bbd659
marcos10pc <30839781+marcos10pc@users.noreply.github.com>
2025-03-02 15:05:09 -0300

    Create imisstheoldkanye.js

---
+++ imisstheoldkanye.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Expansão Noturna Cheeto
+// @namespace https://github.com/marcos10pc/expansao-noturna-cheeto
+// @supportURL      https://github.com/marcos10pc/expansao-noturna-cheeto
+// @version      2025-03-02
+// @description  busque comer cimento
+// @author       marcos10pc
+// @match        https://expansao.educacao.sp.gov.br/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=gov.br
+// @grant        none
+// ==/UserScript==
+
